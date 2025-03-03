489a26199749766781074f5de3455fbd579bdcdb
Sommar-skog <amandaaronsson@hotmail.com>
2025-03-03 15:28:28 +0100

    test builder added

---
+++ src/test/java/org/example/model/businessobject/product/builder/ProductBuilderTest.java
@@ -0,0 +1,28 @@
+package org.example.model.businessobject.product.builder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductBuilderTest {
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void addSize() {
+    }
+
+    @Test
+    void addMaterial() {
+    }
+
+    @Test
+    void addColor() {
+    }
+
+    @Test
+    void build() {
+    }
+}
