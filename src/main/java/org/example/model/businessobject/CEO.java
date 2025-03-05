1a75e37a29cb83707fbe80dc7d6ffd59cd4deeaa
Sommar-skog <amandaaronsson@hotmail.com>
2025-03-05 09:35:37 +0100

    Added Notifier

--- src/main/java/org/example/model/businessobject/CEO.java
+++ src/main/java/org/example/model/businessobject/CEO.java
@@ -5,17 +5,11 @@ import java.beans.PropertyChangeListener;
 
 public class CEO extends Businessobjekt implements PropertyChangeListener {
 
-    //ÄR det OK att göra CEO singleton?
-
     private static CEO instance;
 
     private CEO() {
         name = "Wigell";
     }
-
-    /*private CEO(String name) {
-        super(name);
-    }*/
 
     public static CEO getInstance() {
         if (instance == null) {
---
+++ src/main/java/org/example/model/businessobject/observer/Notifier.java
@@ -0,0 +1,4 @@
+package org.example.model.businessobject.observer;
+
+public class Notifier {
+}
