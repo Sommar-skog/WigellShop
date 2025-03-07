921767c386809243ffe5d77eb19e9ab020dddc9b
Sommar-skog <amandaaronsson@hotmail.com>
2025-03-07 09:58:03 +0100

    updated access modifiers in test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,7 @@
 package org.example;
 
 import org.example.controller.MainController;
-import org.example.model.businessobject.CEO;
-import org.example.model.businessobject.product.Pants;
-import org.example.model.businessobject.product.Skirt;
-import org.example.model.businessobject.product.TShirt;
-import org.example.model.businessobject.product.builder.PantsBuilder;
-import org.example.model.businessobject.product.builder.SkirtBuilder;
-import org.example.model.businessobject.product.builder.TShirtBuilder;
-import org.example.model.businessobject.product.command.*;
-import org.example.model.businessobject.product.enums.*;
+
 
 public class App 
 {
